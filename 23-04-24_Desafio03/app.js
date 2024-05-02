fb21bb6899dc825102f524f5583e3ac6a1bbf73c
Facundo Avalia <109771299+FaMaAvPa@users.noreply.github.com>
2024-05-02 12:40:56 -0300

    tercer desafio

--- 23-04-24_Desafio03/app.js
+++ 23-04-24_Desafio03/app.js
@@ -1,21 +1,37 @@
 import express from 'express';
-import { products } from './products.js'
+import ProductManager from './product.manager.js';
+
+const productManager = new ProductManager('./products.json');
 
 const server = express()
 
-server.get('/products', (req, res)=>{
-    res.json(products)
+server.get('/products', async(req, res)=>{
+    try {
+        const products = await productManager.getProducts();
+        res.json(products)
+        const { limit } = req.query;
+        if (limit) {
+            const productsFilter = products.slice(0, parseInt, (limit));
+            res.status(200).json(productsFilter);
+        } else {
+            res.status(200).json(products);
+        }
+        
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({msg: error.message})
+    }
+    
 
-    const { limit } = req.query;
-    const productsFilter = products.filter()
+    
 })
 
 server.get('/products/:id', (req, res)=>{
 
     const { id } = req.params;
     const prod = products.find(p => p.id === parseInt(id))
-    if(!prod) res.json({msg: 'Product not found'})
-    else res.json(prod)
+    if(!prod) res.status(404).json({msg: 'Product not found'})
+    else res.status(200).json(prod)
 })
 
 const PORT = 8080
